9f5c4753c643b21c9534533d4ca1746a93574db9
root <root@ip-172-31-32-213.us-east-2.compute.internal>
2022-11-24 10:27:45 +0000

    1st

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_instance" "demo_instance" {
+   
+    ami = "ami-0d5bf08bc8017c83b"
+    instance_type = "t2.micro"
+    
+    tags = {
+        Name = "Terraform-Now"
+        
+          }
+}
