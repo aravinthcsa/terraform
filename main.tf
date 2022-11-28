fd8a867805ee3771a0b72ef568424c0482bfd024
aravinth s <103100581+aravinthcsa@users.noreply.github.com>
2022-11-28 16:32:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-provider "aws" {
-    region = "us-east-2"
+#terraform {
+  required_version = "~> 1.2.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.50.0"
+    }
+  }
 }
-
-resource "aws_instance" "demo_instance" {
-   
-    ami = "ami-0d5bf08bc8017c83b"
-    instance_type = "t2.micro"
-    
-    tags = {
-        Name = "Terraform-Now"
-        
-          }
-}
+resource "aws_instance" "my-ec2-vm" {
+  ami               = "ami-0d5bf08bc8017c83b"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-2"
+}#
