72dcd16ef6ef68324b99f101d4471ec8626f39c4
aravinth s <103100581+aravinthcsa@users.noreply.github.com>
2022-11-29 16:06:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3.50.0"
-      region = "us-east-2"
+      version = "3.50.0
     }
   }
 }
 resource "aws_instance" "my-ec2-vm" {
   ami               = "ami-0d5bf08bc8017c83b"
   instance_type     = "t2.micro"
+  region = "us-east-2"
 }
