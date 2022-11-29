d258997756a7dee8e128880168a1873fd98533bf
aravinth s <103100581+aravinthcsa@users.noreply.github.com>
2022-11-29 16:02:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ terraform {
     aws = {
       source = "hashicorp/aws"
       version = "3.50.0"
+      region = "us-east-2"
     }
   }
 }
 resource "aws_instance" "my-ec2-vm" {
   ami               = "ami-0d5bf08bc8017c83b"
   instance_type     = "t2.micro"
-  availability_zone = "us-east-2"
 }
